07725115913cf5e638238edd6d427107820ea3d3
Aleksander Pucher <pucher.aleksander@gmail.com>
2020-12-12 20:53:03 +0100

    solve day 2

--- 2/main.py
+++ 2/main.py
@@ -1,7 +1,8 @@
 with open("input.txt", "r") as f:
     data = [item for item in f.read().splitlines()]
 
-valid_pass_count = 0
+valid_pass_count_1_star = 0
+valid_pass_count_2_star = 0
 for expr in data:
     splitted = expr.split(":")
 
@@ -10,6 +11,14 @@ for expr in data:
     (lower, upper) = splitted[0][:-1].strip().split("-")
 
     if int(lower) <= password.count(letter) <= int(upper):
-        valid_pass_count += 1
+        valid_pass_count_1_star += 1
 
-print(valid_pass_count)
+    if (
+        password[int(lower) - 1] == letter
+        and not password[int(upper) - 1] == letter
+        or not password[int(lower) - 1] == letter
+        and password[int(upper) - 1] == letter
+    ):
+        valid_pass_count_2_star += 1
+
+print(valid_pass_count_1_star, valid_pass_count_2_star)
